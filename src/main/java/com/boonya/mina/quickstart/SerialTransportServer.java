9e76822dee67eb72b9873311288ce7592f7ba069
Allen Peng <boonya@sina.com>
2017-01-11 12:46:55 +0800

    Commit Serial reference！

--- src/main/java/com/boonya/mina/quickstart/SerialTransportServer.java
+++ src/main/java/com/boonya/mina/quickstart/SerialTransportServer.java
@@ -8,11 +8,23 @@ import org.apache.mina.transport.serial.SerialAddress.FlowControl;
 import org.apache.mina.transport.serial.SerialAddress.Parity;
 import org.apache.mina.transport.serial.SerialAddress.StopBits;
 import org.apache.mina.transport.serial.SerialConnector;
-
+/**
+ * 串口通信
+ * 
+ * @package com.boonya.mina.quickstart.SerialTransportServer
+ * @date   2017年1月11日  下午12:40:52
+ * @author pengjunlin
+ * @comment   更多参考：http://blog.csdn.net/yoara/article/details/37726817
+ * @update
+ */
 public class SerialTransportServer {
 	
 	public static void main(String[] args) {
-		IoConnector connector= new SerialConnector();
+		//创建串口连接  
+		//IoConnector connector= new SerialConnector();
+		SerialConnector connector = new SerialConnector(); 
+		
+		//配置串口连接
 		/*SerialAddress portAddress=new SerialAddress("/dev/ttyS0", 38400, 8, StopBits.BITS_1,  Parity.NONE, FlowControl.NONE );
 		
 		ConnectFuture future = connector.connect( portAddress );
